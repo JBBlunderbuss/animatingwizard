320bd0cc48c2c8ce2f84a8b7bfcb306a1a5aef53
James Blundell <j8blundell@gmail.com>
2017-05-01 20:33:53 +1000

    add main behavior

--- app/app.js
+++ app/app.js
@@ -1,8 +1,24 @@
 (function(){
 
-    angular.module('app', [])
+    angular.module('app', ['ngAnimate'])
         .component('mainComponent', {
-            templateUrl: 'app/mainComponent.html'
+            templateUrl: 'app/mainComponent.html',
+            controller: function(){
+                this.viewState = 0
+                this.toggle = function(){
+                    switch (this.viewState){
+                        case 0:
+                            this.viewState = 1; 
+                            break;
+                        case 1:
+                            this.viewState = 2; 
+                            break;
+                        case 2:
+                            this.viewState = 0; 
+                            break;
+                    }
+                }
+            }
         })
 
 }())
