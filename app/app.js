56712f348b409df9f61164210fa321a9a51b463d
James Blundell <j8blundell@gmail.com>
2017-05-01 21:56:53 +1000

    add animation

--- app/app.js
+++ app/app.js
@@ -3,22 +3,34 @@
     angular.module('app', ['ngAnimate'])
         .component('mainComponent', {
             templateUrl: 'app/mainComponent.html',
-            controller: function(){
-                this.viewState = 0
-                this.toggle = function(){
-                    switch (this.viewState){
+            controller: ['$timeout', function($timeout){
+                var vm = this;
+                vm.viewState = 0
+                vm.height = '50px';
+                vm.style = {height: vm.height};
+                vm.toggle = function(){
+                    switch (vm.viewState){
                         case 0:
-                            this.viewState = 1; 
+                            vm.viewState = 1; 
+                            vm.height = '250px'
                             break;
                         case 1:
-                            this.viewState = 2; 
+                            vm.viewState = 2; 
+                            vm.height = '500px'
                             break;
                         case 2:
-                            this.viewState = 0; 
+                            vm.viewState = 0; 
+                            vm.height = '50px'
                             break;
                     }
+                    vm.style = {overflow: 'hidden', height: vm.height}
+                    
+                    $timeout(function(){
+                        console.log('blah')
+                        vm.style = {overflow: 'visible', height: vm.height}
+                    }, 1000)
                 }
-            }
+            }]
         })
 
 }())
